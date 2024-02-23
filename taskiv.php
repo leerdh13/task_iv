f2034608a223f8899a7eca50139a64defe17a014
Ali Ridha <123195042+leerdh13@users.noreply.github.com>
2024-02-23 12:42:29 +0800

    Update taskiv.php

--- taskiv.php
+++ taskiv.php
@@ -24,7 +24,7 @@ foreach($nodes as $i => $node) {
 
     // Only display the article if it was published in 2022 or later
     if ($date && $date->format('Y') >= 2022) {
-        echo "<a href='$url'>$title</a><br>";
+        echo "<a href='$url'>$title</a> <h1>$date</h1> <br>";
     }
 }
 ?>
